7c087ecedb1c1b5880df06b958ffdc3faa1d883a
mikhail chatillon <mikhail.chatillon@hotmail.com>
2018-03-06 16:08:33 +0100

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import face_recognition
 import os
-import cv2
 import json
 from sender import Sender
+import face_recognition
+import picamera
+import numpy as np
 
 def do_face_recognition():
     # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
@@ -14,14 +15,13 @@ def do_face_recognition():
     # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
     # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
-    # Get a reference to webcam #0 (the default one)
-    video_capture = cv2.VideoCapture(0)
+    camera = picamera.PiCamera()
+    camera.resolution = (320, 240)
+    output = np.empty((240, 320, 3), dtype=np.uint8)
 
     # Load a sample picture and learn how to recognize it.
     mik = face_recognition.load_image_file("img/me.jpg")
     mik_face_encodings = face_recognition.face_encodings(mik)[0]
-
-
 
     # Create arrays of known face encodings and their names
     known_face_encodings = [
@@ -36,42 +36,30 @@ def do_face_recognition():
     face_names = []
 
 
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-   # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
-
-
+    print("Capturing image.")
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format="rgb")
     # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(rgb_small_frame)
-    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-    face_names = []
-    for face_encoding in face_encodings:
-    # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "Unknown"
-
-        # If a match was found in known_face_encodings, just use the first one.
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
-            #put processing here
-        face_names.append(name)
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyAllWindows()
-    return face_names[0]
+    face_locations = face_recognition.face_locations(output)
+    print("Found {} faces in image.".format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
+         # Loop over each face found in the frame to see if it's someone we know.
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+            name = "<Unknown Person>"
+            if match[0]:
+                name = "Barack Obama"
+            print("I see someone named {}!".format(name))
+            return face_names[0]
+    return null
+    
 
 
 def main():
     detection_index=0
     # These variables are set by the IoT Edge Agent
-    CONNECTION_STRING = os.getenv('EdgeHubConnectionString', "<no edge aivalable>")
+    CONNECTION_STRING = os.getenv('EdgeHubConnectionString', "HostName=testeriot.azure-devices.net;DeviceId=testmike;SharedAccessKey=kon1VMRcLe/0+D27OnekUues548hdyEU/AIqLlKl6qo=")
     CA_CERTIFICATE = os.getenv('EdgeModuleCACertificateFile', False)
     sender = Sender(CONNECTION_STRING, CA_CERTIFICATE)
     print("connected to "+CONNECTION_STRING)
