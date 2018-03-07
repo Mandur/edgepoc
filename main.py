8549a5845d03255eb556768d621bad04700909d4
mikhail chatillon <mikhail.chatillon@hotmail.com>
2018-03-07 09:01:17 +0100

    tester

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def do_face_recognition():
          # Loop over each face found in the frame to see if it's someone we know.
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+            match = face_recognition.compare_faces([mik_face_encodings], face_encoding)
             name = "<Unknown Person>"
             if match[0]:
                 name = "Barack Obama"
